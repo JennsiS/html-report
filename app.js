977d576a45a18f04acbac76d7b6452373127f95e
JennsiS <san.jenni.r@gmail.com>
2024-10-10 18:00:45 -0600

    feat: right render data

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 document.getElementById('input-excel').addEventListener('change', handleFile);
-document.getElementById('area-salud').addEventListener('change', filterData);
+document.getElementById('area-salud').addEventListener('change', updateMunicipioAndServicio);
 document.getElementById('municipio').addEventListener('change', filterData);
 document.getElementById('servicio-salud').addEventListener('change', filterData);
 
+let mergedData = []; // Store merged data globally
 
 function handleFile(event) {
     const files = event.target.files; // Get the list of files
@@ -67,15 +68,55 @@ function populateSelectors(data) {
     });
 }
 
+function updateMunicipioAndServicio() {
+    const selectedAreas = Array.from(document.getElementById('area-salud').selectedOptions).map(option => option.value);
+    const municipioSelect = document.getElementById('municipio');
+    const servicioSelect = document.getElementById('servicio-salud');
+
+    // Clear existing options
+    municipioSelect.innerHTML = '<option value="">All</option>';
+    servicioSelect.innerHTML = '<option value="">All</option>';
+
+    // Get unique municipios and servicios based on selected areas
+    const filteredMunicipios = new Set();
+    const filteredServicios = new Set();
+
+    mergedData.forEach(item => {
+        if (selectedAreas.includes(item['Área de Salud'])) {
+            filteredMunicipios.add(item['Municipio']);
+            filteredServicios.add(item['Servicio de Salud']);
+        }
+    });
+
+    // Populate municipio select
+    filteredMunicipios.forEach(municipio => {
+        municipioSelect.add(new Option(municipio, municipio));
+    });
+
+    // Populate servicio select
+    filteredServicios.forEach(servicio => {
+        servicioSelect.add(new Option(servicio, servicio));
+    });
+
+    // Trigger filtering of data based on the new selections
+    filterData();
+}
+
 function filterData() {
-    const selectedArea = document.getElementById('area-salud').value;
-    const selectedMunicipio = document.getElementById('municipio').value;
-    const selectedServicio = document.getElementById('servicio-salud').value;
+    console.log("")
+    const selectedAreas = Array.from(document.getElementById('area-salud').selectedOptions).map(option => option.value);
+    const selectedMunicipios = Array.from(document.getElementById('municipio').selectedOptions).map(option => option.value);
+    const selectedServicios = Array.from(document.getElementById('servicio-salud').selectedOptions).map(option => option.value);
 
     const filteredData = mergedData.filter(item => {
-        return (selectedArea === "" || item['Área de Salud'] === selectedArea) &&
-               (selectedMunicipio === "" || item['Municipio'] === selectedMunicipio) &&
-               (selectedServicio === "" || item['Servicio de Salud'] === selectedServicio);
+        const areaMatch = selectedAreas.length === 0 || selectedAreas.includes(item['Área de Salud']);
+        const municipioMatch = selectedMunicipios.length === 0 || selectedMunicipios.includes(item['Municipio']);
+        const servicioMatch = selectedServicios.length === 0 || selectedServicios.includes(item['Servicio de Salud']);
+        return areaMatch && municipioMatch && servicioMatch;
+    });
+
+    filteredData = filteredData.sort((a, b) => {
+        return a.Semana - b.Semana;
     });
 
     renderTable(filteredData); // Render the table with filtered data
@@ -83,70 +124,115 @@ function filterData() {
 }
 
 function renderTable(data) {
-    // Convertir el JSON a un formato de DataTable
-    const columns = Object.keys(data[0]).map(key => ({ title: key, data: key }));
+    // Define custom column names
+    const columnMapping = {
+        'Semana': 'Semana',
+        'Año': 'Año',
+        'Área de Salud': 'Area de Salud',
+        'Municipio': 'Municipio',
+        'Servicio de Salud': 'Servicio de Salud',
+        'Descripción Cie10': 'Descripción',
+        'Métrica': 'Numero de casos'
+    };
+
+    // Convert the JSON to DataTable format with custom column names
+    const columns = Object.keys(data[0]).map(key => ({
+        title: columnMapping[key] || key, // Use custom name if available, otherwise use original
+        data: key
+    }));
     
-    // Agrupar los datos por 'Semana' y 'Descripción Cie10'
     const groupedData = data.reduce((acc, item) => {
         const key = `${item.Semana}-${item['Descripción Cie10']}`;
         if (!acc[key]) {
-            acc[key] = { ...item, count: 0 }; // Contador para cada grupo
+            acc[key] = { ...item, count: 0 }; // Counter for each group
         }
-        acc[key].count += 1; // Incrementar el contador
+        acc[key].count += 1; // Increment the counter
         return acc;
     }, {});
 
-    // Convertir el objeto agrupado de nuevo a un array
+    // Convert the grouped object back to an array
     const finalData = Object.values(groupedData);
 
     $('#data-table').DataTable({
         data: finalData,
         columns: columns,
-        destroy: true,  // Para resetear la tabla si ya existe
+        destroy: true, 
         paging: true,
-        searching: true
+        searching: true,
+        language: {
+            search: "Buscar:",
+            lengthMenu: "Show _MENU_ entries",
+            info: "Mostrando _START_ hasta _END_ de _TOTAL_ resultados",
+            paginate: {
+                first: "Primera",
+                last: "Ultima",
+                next: "Siguiente",
+                previous: "Anterior"
+            }
+        }
     });
 }
 
+
 function renderChart(data) {
-    // Agrupar los datos por 'Semana' y 'Descripción Cie10'
+    // Group the data by 'Descripción Cie10' and week-year
     const groupedData = data.reduce((acc, item) => {
-        const key = item.Semana;
-        if (!acc[key]) {
-            acc[key] = {};
+        const desc = item['Descripción Cie10'];
+        const weekYear = `${item.Semana.toString().padStart(2, '0')}-${item.Año}`;
+
+        if (!acc[desc]) {
+            acc[desc] = {};
         }
-        if (!acc[key][item['Descripción Cie10']]) {
-            acc[key][item['Descripción Cie10']] = 0;
+
+        if (!acc[desc][weekYear]) {
+            acc[desc][weekYear] = 0;
         }
-        acc[key][item['Descripción Cie10']] += 1; // Contar ocurrencias
+
+        acc[desc][weekYear] += item['Métrica'];
         return acc;
     }, {});
 
-    // Preparar los datos para el gráfico
-    const traces = [];
-    Object.keys(groupedData).forEach(semana => {
-        Object.keys(groupedData[semana]).forEach(desc => {
-            const xValues = [];
-            const yValues = [];
-            xValues.push(semana);
-            yValues.push(groupedData[semana][desc]);
-            traces.push({
-                x: xValues,
-                y: yValues,
-                mode: 'scatter',
-                name: desc // Nombre de la serie
-            });
-        });
+    // Get unique week-year combinations and sort them
+    const weekYears = [...new Set(data.map(item => `${item.Semana.toString().padStart(2, '0')}-${item.Año}`))].sort();
+
+    // Prepare the traces (lines) for the chart
+    const traces = Object.keys(groupedData).map(desc => {
+        const x = weekYears;
+        const y = weekYears.map(weekYear => groupedData[desc][weekYear] || 0);
+
+        return {
+            type: 'scatter',
+            mode: 'lines+markers',
+            x: x,
+            y: y,
+            name: desc,
+            line: { shape: 'linear' },
+        };
     });
 
+    // Configuration of the layout
     const layout = {
-        title: 'Gráfico de Tendencia por Semana',
-        xaxis: { title: 'Semana' },
-        yaxis: { title: 'Conteo' }
+        title: 'Tendecia de numero de casos por Semana epidemiologica - Año',
+        xaxis: { 
+            title: 'Semana epidemiologica - Año',
+            tickangle: -45,
+            automargin: true
+        },
+        yaxis: { title: 'Número de casos' },
+        hovermode: 'closest',
+        legend: {
+            orientation: 'h',
+            yanchor: 'bottom',
+            y: -0.3,
+            xanchor: 'center',
+            x: 0.5
+        },
+        margin: {
+            b: 150 // Increase bottom margin to accommodate legend
+        }
     };
 
+    // Render the chart with Plotly
     Plotly.newPlot('chart', traces, layout);
 }
-
-// Automatically load two files and merge their contents
 
